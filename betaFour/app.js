83c9bbda07c4f7c9875ca8d82a2c5c12037bf777
huseynovnabi <131556891+huseynovnabi@users.noreply.github.com>
2023-11-01 15:13:39 +0400

    Add files via upload

---
+++ betaFour/app.js
@@ -0,0 +1,29 @@
+"use stirct"
+
+const h1Element = document.getElementById("movingText");
+const text = h1Element.textContent;
+let currentIndex = 0;
+const delay = 200;
+const eraseDelay = 1000;
+
+function eraseText() {
+    if (currentIndex >= 0) {
+        h1Element.textContent = text.slice(0, currentIndex);
+        currentIndex--;
+        setTimeout(eraseText, delay);
+    } else {
+        setTimeout(writeText, eraseDelay);
+    }
+}
+
+function writeText() {
+    if (currentIndex < text.length) {
+        h1Element.textContent = text.slice(0, currentIndex + 1);
+        currentIndex++;
+        setTimeout(writeText, delay);
+    } else {
+        setTimeout(eraseText, eraseDelay);
+    }
+}
+
+writeText(); // İlk olarak metni yazmaya başla
